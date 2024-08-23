61d7faee9116085b86dd4fb330f61f69a2720dcb
saananokela <smnokela@gmail.com>
2024-08-23 17:13:40 +0300

    Added navigation to the buttons on the HomeScreen.

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,24 +1,27 @@
-import { Text, View } from "react-native";
-import { ButtonStyles, commonStyles } from "../styles/Styles";
+import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { commonStyles } from "../styles/Styles";
 import CustomButton from "../components/CustomButton";
 
 export default function HomeScreen() {
+
+  const navigation = useNavigation();
   return (
     <View style={commonStyles.container}>
       <CustomButton
         title="Lisää harjoitus"
-        onPress={() => console.log('Lisää harjoitus -nappia painettua')}
+        onPress={() => navigation.navigate('Lisää harjoitus')}
         // icon="plus"
         icon="plus-circle-outline"
       />
        <CustomButton
         title="Harjoitushistoria"
-        onPress={() => console.log('Lisää harjoitushistoria -nappia painettua')}
+        onPress={() => navigation.navigate('Harjoitushistoria')}
         icon="calendar-month-outline"
       />
       <CustomButton
         title="Asetukset"
-        onPress={() => console.log('Lisää asetuksia -nappia painettua')}
+        onPress={() => navigation.navigate('Asetukset')}
         icon="cog"
       />
     </View>

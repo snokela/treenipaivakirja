aac63c6057914c3e8bb04e546dce0b86e0e77f72
saananokela <smnokela@gmail.com>
2024-09-04 14:05:42 +0300

    Added simple functionality for display runSum.

--- components/WorkoutHistoryScreen.js
+++ components/WorkoutHistoryScreen.js
@@ -5,16 +5,24 @@ import { FlatList } from "react-native";
 import ExerciseCard from "./ExerciseCard";
 import CustomDivider from "./CustomDivider";
 
+  // TESTIDATAMALLI
+const data = [
+  { id: '1', laji: 'Juoksu', date: '01.08.2024', distance: 5, duration: 40, iconName: 'run-fast' },
+  { id: '2', laji: 'Kävely', date: '02.08.2024', distance: 3, duration: 30, iconName: 'walk' },
+  { id: '3', laji: 'Pyöräily', date: '03.08.2024', distance: 20, duration: 60, iconName: 'bike' },
+  { id: '4', laji: 'Pyöräily', date: '04.08.2024', distance: 3, duration: 10, iconName: 'bike' },
+  { id: '5', laji: 'Pyöräily', date: '05.08.2024', distance: 5, duration: 15, iconName: 'bike' },
+];
+
 export default function WorkoutHistoryScreen() {
 
-  // TESTIDATAMALLI
-  const data = [
-    { id: '1', laji: 'Juoksu', date: '01.08.2024', distance: 5, duration: 40, iconName: 'run-fast' },
-    { id: '2', laji: 'Kävely', date: '02.08.2024', distance: 3, duration: 30, iconName: 'walk' },
-    { id: '3', laji: 'Pyöräily', date: '03.08.2024', distance: 20, duration: 60, iconName: 'bike' },
-    { id: '4', laji: 'Pyöräily', date: '04.08.2024', distance: 3, duration: 10, iconName: 'bike' },
-    { id: '5', laji: 'Pyöräily', date: '05.08.2024', distance: 5, duration: 15, iconName: 'bike' },
-  ];
+  const runSum = data
+    .filter(item => item.laji === 'run')
+    .reduce((sum, item) => sum + item.distance, 0);
+
+
+
+
 
   function Item({ item }) {
     return (

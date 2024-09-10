47069cdd9e0bb398e5ebcad72c3c00bf3bd5f7d8
saananokela <smnokela@gmail.com>
2024-09-10 12:21:34 +0300

    Improved CustomSegmentedButton's code.

--- components/CustomSegmentedButton.js
+++ components/CustomSegmentedButton.js
@@ -1,45 +1,22 @@
 import { SafeAreaView } from "react-native";
 import { SegmentedButtons, useTheme } from "react-native-paper";
 import { customSegmentedButtonStyles } from "../styles/Styles";
-// import { useState } from "react";
-// import { theme } from '../styles/Theme';
-// import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+buttons =  [
+  {value:'run', label: 'Juoksu', icon: 'run-fast'},
+  {value: 'bike', label: 'Pyöräily', icon:'bike'},
+  {value: 'walk', label: 'Kävely',icon: 'walk'}
+]
 
 export default function CustomSegmentedButton({ value, setValue }) {
-  // const [value, setValue] =  useState('')
-//   const theme = useTheme();
-
-// buttons =  [
-//   {value:'run', label: 'Juoksu', icon: 'run-fast'},
-//   {value: 'bike', label: 'Pyöräily', icon:'bike'},
-//   {value: 'walk', label: 'Kävely',icon: 'walk'}
-// ]
 
   return (
     <SafeAreaView style={customSegmentedButtonStyles.container}>
-    {/* <SafeAreaView> */}
       <SegmentedButtons
         style={customSegmentedButtonStyles.buttons}
         value={value}
         onValueChange={setValue}
-        buttons={[
-        {
-          value:'run',
-          label: 'Juoksu',
-          icon: 'run-fast',
-        },
-        {
-          value: 'bike',
-          label: 'Pyöräily',
-          icon:'bike',
-        },
-        {
-          value: 'walk',
-          label: 'Kävely',
-          icon: 'walk',
-
-        },
-       ]}
+        buttons={buttons}
       />
     </SafeAreaView>
   );

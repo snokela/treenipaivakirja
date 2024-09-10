b9db3f33c1feae7dde29766841dd849ea8b60ca0
saananokela <smnokela@gmail.com>
2024-09-10 12:48:41 +0300

    Removed extra images and updated CustomHeaderTitle component.

--- components/CustomHeaderTitle.js
+++ components/CustomHeaderTitle.js
@@ -3,6 +3,7 @@ import { customHeaderStyles } from "../styles/Styles";
 
 
 export default function CustomHeaderTitle() {
+
   return (
     <View style={customHeaderStyles.container}>
       <Text style={customHeaderStyles.title}>Treenipäiväkirja</Text>

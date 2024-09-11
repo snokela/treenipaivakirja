920c41eeee58b20e4fbf123563b65692bc52f21d
saananokela <smnokela@gmail.com>
2024-09-11 10:39:20 +0300

    Fetched workout history data from HistoryDataContext in WorkoutHistoryScreen.

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ import { useState } from 'react';
 export default function App() {
 
   const [workoutHistoryData, setWorkoutHistoryData] = useState([
-    { id: 1, sport: 'Juoksu', date: '01.08.2024', distance: 5, duration: 40, iconName: 'run-fast' },
-    { id: 2, sport: 'Kävely', date: '02.08.2024', distance: 3, duration: 30, iconName: 'walk' },
+    { id: 1, sport: 'Juoksu', date: '01.08.2024', distance: 10, duration: 60, iconName: 'run-fast' },
+    { id: 2, sport: 'Kävely', date: '02.08.2024', distance: 8, duration: 80, iconName: 'walk' },
     { id: 3, sport: 'Pyöräily', date: '03.08.2024', distance: 20, duration: 60, iconName: 'bike' },
     { id: 4, sport: 'Pyöräily', date: '04.08.2024', distance: 3, duration: 10, iconName: 'bike' },
     { id: 5, sport: 'Pyöräily', date: '05.08.2024', distance: 5, duration: 15, iconName: 'bike' },
--- components/WorkoutHistoryScreen.js
+++ components/WorkoutHistoryScreen.js
@@ -4,23 +4,17 @@ import SummaryCard from "../components/SummaryCard";
 import { FlatList } from "react-native";
 import ExerciseCard from "./ExerciseCard";
 import CustomDivider from "./CustomDivider";
-
-
-//HOX!! laji tulee olemaan lopullisessa datamallissa sport!!
-// TESTIDATAMALLI
-const data = [
-  { id: '1', laji: 'Juoksu', date: '01.08.2024', distance: 5, duration: 40, iconName: 'run-fast' },
-  { id: '2', laji: 'Kävely', date: '02.08.2024', distance: 3, duration: 30, iconName: 'walk' },
-  { id: '3', laji: 'Pyöräily', date: '03.08.2024', distance: 20, duration: 60, iconName: 'bike' },
-  { id: '4', laji: 'Pyöräily', date: '04.08.2024', distance: 3, duration: 10, iconName: 'bike' },
-  { id: '5', laji: 'Pyöräily', date: '05.08.2024', distance: 5, duration: 15, iconName: 'bike' },
-];
+import { useContext } from "react";
+import { HistoryDataContext } from "../contexts/WorkoutContext";
 
 export default function WorkoutHistoryScreen() {
+  // haetaan historydata contexstista
+  const historydata = useContext(HistoryDataContext)
+  const data = historydata.workoutHistoryData
 
-  function calculateSportSum(data, laji) {
+  function calculateSportSum(data, sport) {
     return data
-      .filter(item => item.laji === laji)
+      .filter(item => item.sport === sport)
       .reduce((sum, item) => sum + item.distance, 0);
   }
 
